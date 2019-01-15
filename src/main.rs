cbaa9f360851cdf53d21491af8095915d417d4f0
Edward Shen <code@eddie.sh>
2019-01-14 21:45:15 -0500

    Now reads from generated config file

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 #[macro_use]
 extern crate clap;
 extern crate dirs;
+extern crate yaml_rust;
+
 use clap::App;
 
 use std::fs;
+use std::fs::{create_dir_all};
 use std::path::Path;
-fn main() {
+use std::error::Error;
+use yaml_rust::YamlLoader;
+
+fn main() -> Result<(), Box<Error>> {
     let yaml = load_yaml!("cli.yaml");
     let matches = App::from_yaml(yaml)
        .version(crate_version!())
@@ -17,20 +23,29 @@ fn main() {
     let config_dir = config_dir.replace("~",
                                 dirs::home_dir().expect("Could not find home dir!")
                                                 .to_str().expect("Failed to stringify home dir"));
-    init_dotfile_dir(&config_dir);
+    let configs = load_or_init_config(&config_dir)?;
+    println!("{:?}", configs);
+    Ok(())
 }
 
-fn init_dotfile_dir(path: &str) {
-    let config_path = path.to_owned() + "/dotfile.yaml";
+fn load_or_init_config(path: &str) -> Result<Vec<yaml_rust::Yaml>, Box<Error>> {
+    let path = path.to_owned();
+    let config_path = path.clone() + "/dotfile.yaml";
     let resolved_path = Path::new(&config_path).to_str().unwrap();
     let config = match fs::read_to_string(&resolved_path) {
         Ok(conf) => conf,
         Err(_e) => {
-            println!("Generating new configuration file at {}", resolved_path);
             let init_string = format!("version: {}", crate_version!());
-            fs::write(resolved_path, &init_string).expect("Could not write to config file!");
+            match fs::write(resolved_path, &init_string) {
+                Ok(_ok) => _ok,
+                Err(_e) => {
+                    create_dir_all(path)?;
+                    fs::write(resolved_path, &init_string)?;
+                    println!("Generating new configuration file at {}", resolved_path);
+                }
+            };
             init_string
         }
     };
-    println!("{}", config);
+    Ok(YamlLoader::load_from_str(&config)?)
 }
